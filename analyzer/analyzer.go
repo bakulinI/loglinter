c4c47cd2e1d87fb10e6cccbc8d7ad512aee324f0
Ilya Bakulin <ilabakulin77@gmail.com>
2026-02-13 17:11:09 +0300

    simple analyzer

---
+++ analyzer/analyzer.go
@@ -0,0 +1,34 @@
+package analyzer
+
+import (
+	"go/ast"
+
+	"golang.org/x/tools/go/analysis"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "loglint",
+	Doc:  "checks log messages",
+	Run:  run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+
+	for _, file := range pass.Files {
+
+		ast.Inspect(file, func(n ast.Node) bool {
+
+			call, ok := n.(*ast.CallExpr)
+			if !ok {
+				return true
+			}
+
+			// просто выводим все вызовы функций
+			pass.Reportf(call.Pos(), "found call")
+
+			return true
+		})
+	}
+
+	return nil, nil
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module loglinter
+
+go 1.25
+
+require (
+	golang.org/x/mod v0.33.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/tools v0.42.0 // indirect
+)
